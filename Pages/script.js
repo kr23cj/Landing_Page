af2b5d490ffe5dd957078f1bb1957db83d985ad6
Russel Kouame <kr23cj@gmail.com>
2026-04-20 19:01:24 +0000

    Ajout du fichier js

---
+++ Pages/script.js
@@ -0,0 +1,5 @@
+let contact = document.getElementById('ctc');
+contact.addEventListener('click',function(){
+    alert('vous serez contacté dans quelques minutes');
+    console.log('le script c\'est bien déroulé jusqu\'a la fin');
+})
